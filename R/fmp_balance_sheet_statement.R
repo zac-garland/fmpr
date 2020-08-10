bacdee6a19d152fcba1010decd53566d0bdb43d7
zac-garland <zachary.garland@outlook.com>
2020-08-10 08:18:24 -0600

    function renamed

--- R/fmp_balance_sheet_statement.R
+++
@@ -1,7 +0,0 @@
-fmp_balance_sheet_statement <- function(ticker = "AAPL", period = "quarter") {
-  apikey <- fmp_api_key()
-
-  fmp_url <- fmp_url_builder(glue::glue("balance-sheet-statement/{ticker}?period={period}&apikey={apikey}"))
-
-  fmp_data(fmp_url)
-}
